ee5cc8058b84f6b06960c4cbca5c1159b003e1d2
KoushikP04 <paulkoushik04@gmail.com>
2023-03-16 12:52:25 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+import time
+
+driver = webdriver.Chrome(executable_path=r'C:\Users\paulk\Desktop\Selenium\chromedriver.exe')
+
+
+driver.get("https://spot-ef0ee.web.app/checkins")
+
+
+
+
+#to allow you to see objects
+time.sleep(10)
+
+
